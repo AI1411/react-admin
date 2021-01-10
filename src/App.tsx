3891b9057b17a155f6594e393ee38e0890b31f6a
AI1411 <goethe0719@gmail.com>
2021-01-10 15:03:50 +0900

    Router導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav';
-import Menu from './components/Menu';
+import Nav from './secure/Nav';
+import Menu from './secure/Menu';
 import Dashboard from './components/Dashboard';
+import {BrowserRouter, Route} from "react-router-dom";
+import Users from "./components/Users";
+
 
 function App() {
     return (
@@ -13,7 +16,10 @@ function App() {
                 <div className="row">
                     <Menu/>
                     <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-                        <Dashboard/>
+                        <BrowserRouter>
+                            <Route path={'/'} exact component={Dashboard} />
+                            <Route path={'/users'} component={Users} />
+                        </BrowserRouter>
                     </main>
                 </div>
             </div>
