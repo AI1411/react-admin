8898d5a399f1fd8a87e6aa3e0fb24030192e83ed
AI1411 <goethe0719@gmail.com>
2021-01-11 02:29:42 +0900

    ログイン後のユーザ管理

--- src/secure/Wrapper.tsx
+++ src/secure/Wrapper.tsx
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import Nav from "./Nav";
 import Menu from "./Menu";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Redirect, Route} from "react-router-dom";
 import Dashboard from "../components/Dashboard";
 import Users from "../components/Users";
 import Login from "../public/Login";
+import axios from "axios";
 
 class Wrapper extends Component {
+    state = {
+        redirect: false
+    }
+    componentDidMount = async () => {
+        try {
+            const response = await axios.get('user');
+        } catch (e) {
+            this.setState({
+                redirect: true
+            })
+        }
+    }
+
     render() {
+        if (this.state.redirect) {
+            return <Redirect to={'/login'} />
+        }
         return (
             <>
                 <Nav/>
