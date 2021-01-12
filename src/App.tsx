bd595920ac33bc1592184a814234b181c43a013e
AI1411 <goethe0719@gmail.com>
2021-01-13 01:13:57 +0900

    注文一覧、詳細を実装

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ import RoleEdit from "./secure/roles/RoleEdit";
 import Products from "./secure/products/Products";
 import ProductCreate from './secure/products/ProductCreate';
 import ProductEdit from "./secure/products/ProductEdit";
+import Orders from './secure/orders/Orders';
+import OrderItems from './secure/orders/OrderItems';
 
 
 function App() {
@@ -33,6 +35,8 @@ function App() {
                 <Route path={'/products'} exact component={Products}/>
                 <Route path={'/products/create'} exact component={ProductCreate}/>
                 <Route path={'/products/:id/edit'} exact component={ProductEdit}/>
+                <Route path={'/orders'} exact component={Orders}/>
+                <Route path={'/orders/:id'} component={OrderItems} exact/>
             </BrowserRouter>
         </div>
     );
